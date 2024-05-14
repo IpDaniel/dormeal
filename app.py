4abf429b278809776854b21cc9f334701cdc98ee
IpDaniel <ip.d@northeastern.edu>
2024-05-14 17:04:14 -0400

    committing for github push

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, render_template, request
+from Scripts import mvp_emailer
+
+active = True
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/order')
+def order_page():
+    if active:
+        return render_template('order.html')
+    else:
+        return render_template('inactive.html')
+
+@app.route('/submit-order', methods=['POST'])
+def submit_order():
+    data = request.json
+    print('New Order Placed:')
+    print(data)
+    email_list = mvp_emailer.order_notify_email_list
+    mvp_emailer.send_email_to_multiple(email_list, str(data))
+    return data
+
+
+app.run()
